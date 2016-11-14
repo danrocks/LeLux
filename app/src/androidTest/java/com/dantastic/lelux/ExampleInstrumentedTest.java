8352486b8597c27ba056536f382ac7b15495576e
Daniel <superstardk@gmail.com>
2016-11-14 17:39:58 +0000

    Changed packagename since I couldn'y get dantastic.com but could get dandabby.com.
    I believe I know have the boot receiver working as well as having event handlers properly wired.
    
    Need to get the broadcast receiver going next.

--- app/src/androidTest/java/com/dantastic/lelux/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/dantastic/lelux/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.dantastic.lelux;
+package com.dandabby.lelux;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.dantastic.lelux", appContext.getPackageName());
+        assertEquals("com.dandabby.lelux", appContext.getPackageName());
     }
 }
